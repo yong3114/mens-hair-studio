92d0c163a3c314280b33013ef7267fef06dac5db
yong3114 <82628618+yong3114@users.noreply.github.com>
2026-09-23 22:21:15 +0800

    QA: add customer app entry point

---
+++ src/customer-main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CustomerApp from './CustomerApp.jsx'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+  <CustomerApp/>
+ </React.StrictMode>
+)
